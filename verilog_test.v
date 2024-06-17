19570f36f70b58e8e2996144e7ce71dcf5741c03
Ben Landon <149592168+Q5wan@users.noreply.github.com>
2024-06-17 17:05:50 +0200

    ALU

--- verilog_test.v
+++ verilog_test.v
@@ -1,5 +1,5 @@
-module alu_4bits (
-   input  wire [7:0] ui_in,    // Dedicated inputs
+module Q5wan_4_bit_ALU (
+    input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
@@ -8,28 +8,40 @@ module alu_4bits (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
- 
+  /*on the positive edge of the clock put the first 8 bits into either A or B based on
+  which the state of AB*/
   reg [7:0] A, B;
   wire [7:0] AB;
-  assign AB = ui_in & 8'b0001_0000;
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
       A <= 8'b0;
       B <= 8'b0;
-    end else if (AB == 8'b0001_0000) begin
+    end else
       A <= ui_in & 8'b0000_1111;
-    end else begin
-      B <= ui_in & 8'b0000_1111;
-    end
+      B <= (ui_in & 8'b1111_0000) >> 4;
   end
   
-  wire [7:0]operation;
-  assign operation = ui_in & 8'b1110_0000;
+  /*on the negative edge of the clock output the set Y to */
+  wire [7:0] operation;
+  reg [7:0] Y;
+  always @(negedge clk) begin
+    operation <= (uio_in & 8'b0000_0111);
+    case (operation)
+      8'b0000_0000 : Y <= A+B;
+      8'b0000_0001 : Y <= A-B;
+      8'b0000_0010 : Y <= A&B;
+      8'b0000_0011 : Y <= A|B;
+      8'b0000_0100 : Y <= A^B;
+      8'b0000_0101 : Y <= ~A;
+      8'b0000_0110 : Y <= A>>1;
+      8'b0000_0111 : Y <= A<<1;
+      default : Y <= 8'b0;
+    endcase
+  end
   
-
-
+  
   // Assign other outputs based on the design requirements
-  assign uo_out = 8'b0; // Placeholder, replace with actual logic
+  assign uo_out = Y; // Placeholder, replace with actual logic
   assign uio_out = 8'b0; // Placeholder, replace with actual logic
   assign uio_oe = 8'b0; // Placeholder, replace with actual logic
 
